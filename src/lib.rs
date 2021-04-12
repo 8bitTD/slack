0f63c26166bcd72126d1667a57b83bf8be8ad1bb
8bit <freeman1218@gmail.com>
2021-04-12 23:47:38 +0900

    remove

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
-pub mod slack;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+use urlencoding::encode;
+pub fn send_slack(tkn:&str, cnl:&str, usr:&str, msg:&str) -> std::io::Result<()> {
+    let mut cmd: String = "curl -XPOST -d token=".to_string();
+    cmd = cmd + &tkn;
+    cmd = cmd + " -d channel=";
+    cmd = cmd + &cnl + " -d text=";
+    cmd = cmd + &encode(&msg) + " -d username=" + &usr + " -d icon_url=icon_emoji ";
+    cmd = cmd + "https://slack.com/api/chat.postMessage";
+    std::process::Command::new("cmd.exe").args(&["/C", &cmd]).output().expect("failed to execute process");
+    Ok(())
 }
