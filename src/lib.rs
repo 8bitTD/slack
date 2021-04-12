6ac99074b5738452fabf677fe327deadf28bae87
8bit <freeman1218@gmail.com>
2021-04-13 01:18:42 +0900

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -7,5 +7,6 @@ pub fn send_slack(tkn:&str, cnl:&str, usr:&str, msg:&str) -> std::io::Result<()>
     cmd = cmd + &encode(&msg) + " -d username=" + &usr + " -d icon_url=icon_emoji ";
     cmd = cmd + "https://slack.com/api/chat.postMessage";
     let res = std::process::Command::new("cmd.exe").args(&["/C", &cmd]).output().expect("failed to execute process");
-    Ok(res)
+    println!("{:?}", res);
+    Ok()
 }
