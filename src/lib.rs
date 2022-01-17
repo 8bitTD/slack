a64f0c45e410eb781e9aefc1671e10a7a50aa64e
8bit <freeman1218@gmail.com>
2022-01-17 14:25:49 +0900

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 use urlencoding::encode;
+use std::os::windows::process::CommandExt;
+const CREATE_NO_WINDOW: u32 = 0x08000000;
 pub fn send_slack(tkn:&str, cnl:&str, usr:&str, msg:&str) -> std::io::Result<()> {
     let mut cmd: String = "curl -XPOST -d token=".to_string();
     cmd = cmd + &tkn;
@@ -6,6 +8,6 @@ pub fn send_slack(tkn:&str, cnl:&str, usr:&str, msg:&str) -> std::io::Result<()>
     cmd = cmd + &cnl + " -d text=";
     cmd = cmd + &encode(&msg) + " -d username=" + &usr + " -d icon_url=icon_emoji ";
     cmd = cmd + "https://slack.com/api/chat.postMessage";
-    let res = std::process::Command::new("cmd.exe").args(&["/C", &cmd]).output().expect("failed to execute process");
+    let res = std::process::Command::new("cmd.exe").creation_flags(CREATE_NO_WINDOW).args(&["/C", &cmd]).output().expect("failed to execute process");
     Ok(())
 }
